9b9e8fbdb4edea4ded01c19cd4191268a5c84054
Shane Stock <shane@shanestock.dev>
2023-11-07 12:38:49 -0500

    Add video and autoplay when in view

--- scripts/main_site.js
+++ scripts/main_site.js
@@ -1,10 +1,37 @@
-window.onload = function() {
+var video;
+var fraction = 0.8;
+
+document.addEventListener("DOMContentLoaded", function() {
     var images = document.querySelectorAll('#image-matrix img');
     var classes = ['slideInFromLeft', 'slideInFromRight', 'slideInFromTop', 'slideInFromBottom'];
 
-    images.forEach(function(image) {
+    images.forEach(function (image) {
         var randomIndex = Math.floor(Math.random() * classes.length);
         var randomClass = classes[randomIndex];
         image.classList.add(randomClass);
     });
-};
+
+    video = document.getElementById("demo1");
+});
+
+function checkScroll() {
+    if (!video) return; // If video is still not defined, exit the function
+
+    var x = video.offsetLeft, y = video.offsetTop, w = video.offsetWidth, h = video.offsetHeight, r = x + w, //right
+        b = y + h, //bottom
+        visibleX, visibleY, visible;
+
+    visibleX = Math.max(0, Math.min(w, window.scrollX + window.innerWidth - x, r - window.scrollX));
+    visibleY = Math.max(0, Math.min(h, window.scrollY + window.innerHeight - y, b - window.scrollY));
+
+    visible = visibleX * visibleY / (w * h);
+
+    if (visible > fraction) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+window.addEventListener('scroll', checkScroll, false);
+window.addEventListener('resize', checkScroll, false);
